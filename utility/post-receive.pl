c45edec08309fdabe94f199a73f410ca27d1f284
lucarin91 <lucarin91@gmail.com>
2016-06-30 12:25:27 +0200

    update post-receive script

--- utility/post-receive.pl
+++ utility/post-receive.pl
@@ -4,76 +4,82 @@
 
 use Config::Simple;
 use FindBin;
-$AbsPath = $FindBin::RealBin.'/';
+$AbsPath = $FindBin::RealBin . '/';
 
-die "parameter error\n" unless ($#ARGV == 0);
+die "parameter error\n" unless ( $#ARGV == 0 );
 $rootDir = $ARGV[0];
 
-Config::Simple->import_from("${AbsPath}post-receive.conf", \%cfg);
-$virtualenv = $cfg{'VIRTUALENV_PATH'} or chomp ($virtualenv=`which virtualenv`);
-$npm = $cfg{'NPM_PATH'} or chomp ($npm=`which npm`);
+Config::Simple->import_from( "${AbsPath}post-receive.conf", \%cfg );
+$virtualenv = $cfg{'VIRTUALENV_PATH'}
+  or chomp( $virtualenv = `which virtualenv` );
+$npm = $cfg{'NPM_PATH'} or chomp( $npm = `which npm` );
 
-$runtimeFile = $rootDir.'/runtime.txt';
-if (-e $runtimeFile) {
-  #%cfgRuntime = $cfg->vars();
-  open RUNTIME, "<", $runtimeFile or die $!;
-  $language = <RUNTIME>;
-  chomp $language;
-  #print "language: $language , $cfgRuntime{$language}\n";
-  $runtime = $cfg{$language}
-    or chomp ($runtime = `which $language`)
-    or die "error runtime $language not found\n";
+$runtimeFile = $rootDir . '/runtime.txt';
+if ( -e $runtimeFile ) {
 
-  print "found runtime $runtime\n";
+    # %cfgRuntime = $cfg->vars();
+    open RUNTIME, "<", $runtimeFile or die $!;
+    $language = <RUNTIME>;
+    chomp $language;
 
-  $requirements = $rootDir.'/requirements.txt';
-  $package = $rootDir.'/package.json';
-  if (-e $requirements) {
-    python();
-  }elsif (-e $package){
-    node();
-  }else{
-    die "no package.json or requirement.txt found\n";
-  }
+    # print "language: $language , $cfgRuntime{$language}\n";
+    $runtime =
+         $cfg{$language}
+      or chomp( $runtime = `which $language` )
+      or die "error runtime $language not found\n";
 
-  if (! -e $rootDir.'/public'){
-    system("mkdir ${rootDir}/public");
-    if( $? == 0){
-      print "public directory created\n"
+    print "found runtime $runtime\n";
+
+    $requirements = $rootDir . '/requirements.txt';
+    $package      = $rootDir . '/package.json';
+    if ( -e $requirements ) {
+        python();
     }
-  }
-
-  if (! -e $rootDir.'/tmp'){
-    system("mkdir ${rootDir}/tmp");
-    if( $? == 0){
-      print "tmp directory created\n"
+    elsif ( -e $package ) {
+        node();
     }
-  }
-  system("touch $rootDir/tmp/restart.txt");
+    else {
+        die "no package.json or requirement.txt found\n";
+    }
 
-}else{
-  print "found HTML\n";
+    if ( !-e $rootDir . '/public' ) {
+        system("mkdir ${rootDir}/public");
+        if ( $? == 0 ) {
+            print "public directory created\n";
+        }
+    }
+
+    if ( !-e $rootDir . '/tmp' ) {
+        system("mkdir ${rootDir}/tmp");
+        if ( $? == 0 ) {
+            print "tmp directory created\n";
+        }
+    }
+    system("touch $rootDir/tmp/restart.txt");
+
+}
+else {
+    print "found HTML\n";
 }
 
-sub python{
-  print "found python\n";
-  my $venv = $rootDir.'/venv';
-  system("$virtualenv -p $runtime $venv");
-  if ( $? == 0 ){
-    my $pip = $venv.'/bin/pip';
+sub python {
+    print "found python\n";
+    my $venv = $rootDir . '/venv';
+    if ( !-d $venv ) {
+        system("$virtualenv -p $runtime $venv");
+        die "can't create virtualenv\n" unless $? != 0;
+    }
+    my $pip = $venv . '/bin/pip';
     system("$pip install -r $requirements");
-    if ( $? != 0 ){
-      die "error pip install\n";
+    if ( $? != 0 ) {
+        die "error pip install\n";
     }
-  }else {
-    die "can't create virtualenv\n";
-  }
 }
 
-sub node{
-  print "found nodejs\n";
-  system("$npm install --nodedir=$runtime --prefix $rootDir");
-  if ($? != 0){
-    die "error npm install\n";
-  }
+sub node {
+    print "found nodejs\n";
+    system("$npm install --nodedir=$runtime --prefix $rootDir");
+    if ( $? != 0 ) {
+        die "error npm install\n";
+    }
 }
